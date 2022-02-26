a66b002bc62d8db6ad5b07751bdf1ed7af8c05f8
jun <watanabejapan2trap@gmail.com>
2022-02-26 17:31:08 +0900

    取得モーションの反映途中

--- Assets/Script/GetMotionList.cs
+++ Assets/Script/GetMotionList.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GetMotionList : MonoBehaviour
 {
     public List<string> motionList = new List<string>();
+    Animator anim;
 
     void OnCollisionEnter(Collision other)
     {
@@ -24,4 +25,41 @@ public class GetMotionList : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //        if (Input.GetKeyDown(KeyCode.A) && !anim.GetCurrentAnimatorStateInfo(0).IsName("Punch") && !anim.IsInTransition(0))
+        if (!Input.GetKeyDown(KeyCode.A) &&
+            !Input.GetKey(KeyCode.UpArrow) && 
+            !Input.GetKey(KeyCode.DownArrow) && 
+            !Input.GetKey(KeyCode.RightArrow) && 
+            !Input.GetKey(KeyCode.LeftArrow) &&
+            (!anim.GetBool("Dance01") ||
+             !anim.GetBool("Dance02")
+             )
+            &&
+            !Input.GetKey(KeyCode.Space)
+            )
+        {
+            foreach(string s in motionList)
+            {
+                string str = s.Substring(10);
+                Debug.Log(str);
+                anim.SetBool(str, true);
+            }
+        }
+        else
+        {
+            foreach (string s in motionList)
+            {
+                string str = s.Substring(10);
+                anim.SetBool(str, false);
+            }
+        }
+    }
+
 }
