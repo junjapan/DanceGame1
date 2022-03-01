1b8194c7b62df18403afa8ba35c6f01059b9a870
hogehoge <susumitosusyo@hotmail.com>
2022-03-01 12:37:01 +0900

    モーション表示

---
+++ Assets/Script/Actions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Actions : MonoBehaviour
+{
+    float countTime = 0;
+
+    public GameObject player;
+    public GetMotionList motions;
+
+    void Start()
+    {
+        motions = player.gameObject.GetComponent<GetMotionList>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        List<string> actions = motions.motionList;
+
+        string[] array = actions.ToArray();
+
+        for (int i = 0; i <= array.Length; i++)
+        {
+
+            GetComponent<Text>().text = string.Join("\n", array);
+        }
+        Debug.Log(string.Join("\n", actions));
+
+
+        //得たモーションをデバッグ表示する。
+
+        Debug.Log(string.Join(",", actions));
+
+        Debug.Log(System.Tuple.Create(actions));
+
+        Debug.Log("Guess What");
+
+
+    }
+
+
+}
