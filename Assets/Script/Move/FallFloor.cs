926811527ad082dbe5d15bda087cefe68efeb340
kasamatsu <kasamatsu.shoji@gmail.com>
2022-03-03 22:02:28 +0900

    敵キャラ用アセット複数追加

---
+++ Assets/Script/Move/FallFloor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallFloor : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Invoke("Fall", 1);
+            Invoke("FallStop", 5);
+        }
+    }
+
+    void Fall()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    void FallStop()
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
