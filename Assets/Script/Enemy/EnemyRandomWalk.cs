b62e1e547318e3367e241d97dbe0a4c3711381ef
kasamatsu <kasamatsu.shoji@gmail.com>
2022-03-05 05:40:43 +0900

    敵キャラの動きスクリプト作成

---
+++ Assets/Script/Enemy/EnemyRandomWalk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRandomWalk : MonoBehaviour
+{
+    float chargeTime = 5.0f;
+    float timeCount;
+
+    void Update()
+    {
+        timeCount += Time.deltaTime;
+
+        transform.position += transform.forward * Time.deltaTime;
+
+        if (timeCount > chargeTime)
+        {
+            Vector3 course = new Vector3(0,Random.Range(0, 360), 0);
+            transform.localRotation = Quaternion.Euler(course);
+
+            timeCount = 0;
+        }
+    }
+}
---
+++ Assets/Script/Enemy/EnemyTeleportation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTeleportation : MonoBehaviour
+{
+    public float moveTime = 3;
+    float vecX;
+    float vecZ;
+    Vector3 startPosition; 
+
+    void Start()
+    {
+        startPosition = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        moveTime -= Time.deltaTime;
+
+        if (moveTime <= 0)
+        {
+            vecX = Random.Range(-5.0f, 5.0f);
+            vecZ = Random.Range(-5.0f, 5.0f);
+            gameObject.transform.position = startPosition + new Vector3(vecX, 0.5f, vecZ);
+            moveTime = 3.0f;
+        }
+    }
+}
