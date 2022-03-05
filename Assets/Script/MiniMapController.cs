985adb2c006876eedb05495423d6f47de66063a7
kasamatsu <kasamatsu.shoji@gmail.com>
2022-03-05 17:25:08 +0900

    ミニマップ追加

---
+++ Assets/Script/MiniMapController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapController : MonoBehaviour
+{
+    public void MiniMapOn()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void MiniMapOff()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Script/MiniMapOperation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapOperation : MonoBehaviour
+{
+    public GameObject miniMapImage;
+
+    void Start()
+    {
+        miniMapImage = GameObject.Find("MiniMapImage");
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.V)) //Vボタン
+        {
+            miniMapImage.GetComponent<MiniMapController>().MiniMapOn();
+        }   
+
+        if (Input.GetKey(KeyCode.B)) //Bボタン
+        {
+            miniMapImage.GetComponent<MiniMapController>().MiniMapOff();
+        }
+            
+    }
+}
