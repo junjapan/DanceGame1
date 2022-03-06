38a2ecc6f4ccc1463324db08a09de53facd92882
kasamatsu <kasamatsu.shoji@gmail.com>
2022-03-06 18:24:19 +0900

    敵の配置、ステージ調整

---
+++ Assets/Script/Enemy/EnemyRun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRun : MonoBehaviour
+{
+    int counter = 0;
+    public int range;
+    float move = 0.1f;
+    Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 p = new Vector3(0, 0, move);
+        transform.Translate(p);
+        counter++;
+
+        if (counter == range)
+        {
+            counter = 0;
+            transform.position = startPosition;
+        }
+    }
+}
